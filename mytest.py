7fc80e018f31c3a4abf4e961b5254c0dbb6c3bd8
Leon16226 <644164834@qq.com>
2021-12-09 10:55:56 +0800

    update

--- mytest.py
+++ mytest.py
@@ -3,5 +3,4 @@ import numpy as np
 import json
 
 if __name__ == '__main__':
-   client_date = ""
-   user_dic = json.loads(client_date)
+   file = open('roadClose.log', 'w')
---
+++ test/setup.py
@@ -0,0 +1,5 @@
+# file : setup.py
+from distutils.core import setup
+from Cython.Build import cythonize
+
+setup(name='Hello world app', ext_modules=cythonize("hello.py"))
